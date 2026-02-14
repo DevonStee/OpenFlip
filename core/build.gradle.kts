2e3dfab6dccaeb12488e72f029c493075fbee242
DevonStee <yanjiaqiid@gmail.com>
2026-02-14 05:25:05 -0500

    fix(core): add missing material dependency for theme resources

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -22,6 +22,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.annotation)
+    implementation(libs.material)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.javax.inject)
 }
