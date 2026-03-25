f1d7b4c0ff99d7482e0ed9a7d6ad9fd592a9abdc
DevonStee <yanjiaqiid@gmail.com>
2026-03-25 14:22:03 -0400

    chore(license): add GPL-3.0 license header to all Kotlin source files
    Required by F-Droid review for GPL-3.0-or-later compliance.

--- app/src/main/java/com/bokehforu/openflip/widget/debug/WidgetLeakDebugHelper.kt
+++ app/src/main/java/com/bokehforu/openflip/widget/debug/WidgetLeakDebugHelper.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2026 DevonStee
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.bokehforu.openflip.widget.debug
 
 import android.content.Context
@@ -20,7 +37,7 @@ object WidgetLeakDebugHelper {
      * Tracks update frequency to detect potential memory issues.
      */
     fun logWidgetUpdate(context: Context, widgetId: Int, providerClass: Class<*>) {
-        initDebugPrefs(context.applicationContext)
+        if (isDebuggable == null) initDebugPrefs(context.applicationContext)
         if (isDebuggable != true) return
 
         val count = (widgetStats[widgetId] ?: 0) + 1
